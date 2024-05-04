3426e5f95e79597f974c1dcdf5f6503bee49f54b
Khairul Aqram <khairulaqram21@gmail.com>
2024-05-04 14:59:08 +0700

    Update MonJaTir Project

---
+++ Send_HTTP_Web/http_web_esp8266/http_web_esp8266.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h> // include library DHT22 Wifi
+#include <ESP8266HTTPClient.h>
+
+// konfigurasi Wifi
+const char* ssid = "POCCO";
+const char* password = "kipasangin1234";
+
+// alamat ip server
+const char* server = "192.168.100.28";
+ 
+void setup() {
+  Serial.begin(9600); 
+
+  // Koneksi Ke Wifi
+  WiFi.hostname("NodeMCU");
+  WiFi.begin(ssid, password);
+
+  // uji koneksi wifi
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Wifi gagal terkoneksi");
+    delay(500);
+  }
+
+  // apabila berhasil konek
+    Serial.println("Wifi berhasil terkoneksi");
+}
+
+void loop() {
+
+  // Pengiriman data sensor
+  WiFiClient wClient ;
+  // Cek koneksi ke web server
+  const int httpPort = 80;
+  if(!wClient.connect(server, httpPort)) 
+  {
+    Serial.println("Gagal Konek Ke Web Server");
+    return;
+  }
+
+  // Proses pengiriman data
+  String url;
+  HTTPClient http; // protokol http
+  url = "http://" + String(server) + "/mushroom-monitoring-web/public/simpan/20/32/huhu/haha";
+  // url = "https://hydroponic.fun/pages/urlData/30/30/Hallo/Hallo/Hallo";
+
+  // Eksekusi url
+  http.begin(wClient, url);
+  http.GET();
+  http.end();
+  Serial.println("Berhasil mengirim data");
+
+  delay(2000); //Collecting period should be : >1.7 second
+}
--- monitoriing_jamur_tiram/monJamurTiram/monJamurTiram.ino
+++ monitoriing_jamur_tiram/monJamurTiram/monJamurTiram.ino
@@ -1,4 +1,6 @@
-#include <DHT22.h>
+#include <DHT22.h> // include library DHT22
+#include <ESP8266WiFi.h> // include library DHT22 Wifi
+#include <ESP8266HTTPClient.h>
 //define pin data dht22
 #define pinDATA 13 // SDA, or almost any other I/O pin
 // Mendefinisikan pin relay
@@ -6,17 +8,40 @@ const int relayKipas = 16;
 const int relayAtomizer = 5;
 
 DHT22 dht22(pinDATA); 
+
+// konfigurasi Wifi
+const char* ssid = "POCCO";
+const char* password = "kipasangin1234";
+
+// alamat ip server
+const char* server = "192.168.100.28";
  
 void setup() {
   Serial.begin(9600); 
   pinMode(relayKipas, OUTPUT);
   pinMode(relayAtomizer, OUTPUT);
+
+  // Koneksi Ke Wifi
+  WiFi.hostname("NodeMCU");
+  WiFi.begin(ssid, password);
+
+  // uji koneksi wifi
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println("Wifi gagal terkoneksi");
+    delay(500);
+  }
+
+  // apabila berhasil konek
+    Serial.println("Wifi berhasil terkoneksi");
 }
 
 void loop() {
 
   float t = dht22.getTemperature();
   float h = dht22.getHumidity();
+  String kipas;
+  String atomizer;
 
   Serial.print("Kelembapan: ");
   Serial.print(h, 1);
@@ -28,19 +53,42 @@ void loop() {
   if (t > 30.0) {
     digitalWrite(relayKipas, LOW); // Turn on the fan relay
     Serial.println("Kipas dinyalakan."); // Print message that fan is turned on
+    kipas = "Hidup";
   } else {
     digitalWrite(relayKipas, HIGH); // Turn off the fan relay
     Serial.println("Kipas dimatikan."); // Print message that fan is turned off
+    kipas = "Mati";
   }
 
   // Control relayAtomizer based on humidity
   if (h < 80.0) {
     digitalWrite(relayAtomizer, LOW); // Turn on the atomizer relay
     Serial.println("Atomizer dinyalakan."); // Print message that atomizer is turned on
-  } else if (h > 90.0) {
+    atomizer = "Hidup";
+  } else {
     digitalWrite(relayAtomizer, HIGH); // Turn off the atomizer relay
     Serial.println("Atomizer dimatikan."); // Print message that atomizer is turned off
+    atomizer = "Mati";
   }
+
+  // Pengiriman data sensor
+  WiFiClient wClient ;
+  // Cek koneksi
+  const int httpPort = 80;
+  if(!wClient.connect(server, httpPort)) {
+    Serial.println("Gagal Konek Ke Web Server");
+    return;
+  }
+
+  // Proses pengiriman data
+  String url;
+  HTTPClient http; // protokol http
+  url = "http://" + String(server) + "/mushroom-monitoring-web/public/simpan/" + String(t) + "/" + String(h) + "/" + kipas + "/" + atomizer;
+
+  // Eksekusi url
+  http.begin(wClient, url);
+  http.GET();
+  http.end();
 
   delay(2000); //Collecting period should be : >1.7 second
 }
